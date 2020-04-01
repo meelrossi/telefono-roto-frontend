8843a77ab852b0d600f77653c182f50ce8ba4b8c
Santiago Acuña <santiago.acuna@wolox.com.ar>
2020-03-31 21:11:42 -0300

    Style canvas on Game screen

--- src/screens/Game/Game.js
+++ src/screens/Game/Game.js
@@ -4,22 +4,35 @@ import CanvasDraw from "react-canvas-draw";
 import './Game.scss';
 
 export default class Game extends Component {
+
+  clearCanvas = () => {
+    this.canvas.clear();
+  }
+
+  undoCanvas = () => {
+    this.canvas.undo();
+  }
+
   render() {
     const height = window.outerHeight > 550 ? 550 : window.outerHeight - 40;
     const width = window.outerWidth > 400 ? 400 : window.outerWidth - 40;
     return (
       <div className="container">
-        <div className="canvas-container">
+        <div id='canvas-container' className="canvas-container">
           <div className="game-info">
-            <span>00:20</span>
-            <button onClick={() => console.log("undo")}>
-              Undo
-            </button>
-            <button onClick={() => console.log("clear")}>
-              Clear
-            </button>
+            <div className='button-container'>
+              <button className="action-button" onClick={this.undoCanvas}>
+                Undo
+              </button>
+              <button className="action-button"  onClick={this.clearCanvas}>
+                Clear
+              </button>
+            </div>
+            
+            <span className="timer">00:20</span>
           </div>
           <CanvasDraw
+            ref={canvasDraw => this.canvas = canvasDraw}
             hideGrid
             brushRadius={3}
             lazyRadius={0}
