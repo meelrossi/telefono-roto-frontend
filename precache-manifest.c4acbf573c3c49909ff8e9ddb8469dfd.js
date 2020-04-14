a6c80b8f8bb08dd079dfd823a0997fc2a9a83084
Santiago Acuña <santiago.acuna@wolox.com.ar>
2020-04-13 21:36:18 -0300

    Updates

---
+++ precache-manifest.c4acbf573c3c49909ff8e9ddb8469dfd.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd158ef39af03be28add09716b54aa6",
+    "url": "/telefono-roto-frontend/index.html"
+  },
+  {
+    "revision": "6c49211f8a4aa65b200e",
+    "url": "/telefono-roto-frontend/static/css/main.803f8a35.chunk.css"
+  },
+  {
+    "revision": "91a45f650ff874f72dc2",
+    "url": "/telefono-roto-frontend/static/js/2.62e89611.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/telefono-roto-frontend/static/js/2.62e89611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c49211f8a4aa65b200e",
+    "url": "/telefono-roto-frontend/static/js/main.10af91c7.chunk.js"
+  },
+  {
+    "revision": "a91956a1680ce8b9fcc2",
+    "url": "/telefono-roto-frontend/static/js/runtime-main.7ae49851.js"
+  },
+  {
+    "revision": "28e6ad1357c8380cfb42e2b9fbd8d9cc",
+    "url": "/telefono-roto-frontend/static/media/TrashHand.28e6ad13.ttf"
+  },
+  {
+    "revision": "aa31cfcb44810436f52e0153a692dcd2",
+    "url": "/telefono-roto-frontend/static/media/animals.aa31cfcb.png"
+  },
+  {
+    "revision": "92b1b6fb30d8d5f1d76c787a8f59ca7b",
+    "url": "/telefono-roto-frontend/static/media/bat.92b1b6fb.png"
+  },
+  {
+    "revision": "7dd6c46e6737238ba531ad273ea672a5",
+    "url": "/telefono-roto-frontend/static/media/cat.7dd6c46e.png"
+  },
+  {
+    "revision": "e1fe689b00083923c425a6291a0226f6",
+    "url": "/telefono-roto-frontend/static/media/chicken.e1fe689b.png"
+  },
+  {
+    "revision": "5a87ccb789276e07af53efdcf3db875a",
+    "url": "/telefono-roto-frontend/static/media/coronavirus_draw.5a87ccb7.png"
+  },
+  {
+    "revision": "59e4f776f88fdaa007948cf055eae9ec",
+    "url": "/telefono-roto-frontend/static/media/dog.59e4f776.png"
+  },
+  {
+    "revision": "ef0969c67495ce7414798d700c472b87",
+    "url": "/telefono-roto-frontend/static/media/elephant.ef0969c6.png"
+  },
+  {
+    "revision": "d25163bd3d753a83023a6991d4759ac5",
+    "url": "/telefono-roto-frontend/static/media/fox.d25163bd.png"
+  },
+  {
+    "revision": "a1a6ac638756b59bbdc56a6eb65e0d57",
+    "url": "/telefono-roto-frontend/static/media/house_draw.a1a6ac63.png"
+  },
+  {
+    "revision": "86a87edc38d3391b4d207c7f4139bd5e",
+    "url": "/telefono-roto-frontend/static/media/jellyfish.86a87edc.png"
+  },
+  {
+    "revision": "fa12c7b178dfca56367288977f907f45",
+    "url": "/telefono-roto-frontend/static/media/meerkat.fa12c7b1.png"
+  },
+  {
+    "revision": "64c5e8ed28874fa2fb80bb9958450bc8",
+    "url": "/telefono-roto-frontend/static/media/monkey.64c5e8ed.png"
+  },
+  {
+    "revision": "a64ff94d7d77c1d84c39f392389174af",
+    "url": "/telefono-roto-frontend/static/media/octopus.a64ff94d.png"
+  },
+  {
+    "revision": "e6106e638ed6f7b5a14884e17d26f9d8",
+    "url": "/telefono-roto-frontend/static/media/owl.e6106e63.png"
+  },
+  {
+    "revision": "bb45be55f3948c70197dba959896355f",
+    "url": "/telefono-roto-frontend/static/media/owl_draw.bb45be55.png"
+  },
+  {
+    "revision": "56a186662def6f010efd27c8a24bed8d",
+    "url": "/telefono-roto-frontend/static/media/penguin.56a18666.png"
+  },
+  {
+    "revision": "8bf5ae0edef89da4116df3e802477df4",
+    "url": "/telefono-roto-frontend/static/media/pig.8bf5ae0e.png"
+  },
+  {
+    "revision": "4ac99bb1cdc51a38ff2b21ad1f78ee0a",
+    "url": "/telefono-roto-frontend/static/media/rabbit.4ac99bb1.png"
+  },
+  {
+    "revision": "a09e9eb8c201d1cb1ecaa90f661120f6",
+    "url": "/telefono-roto-frontend/static/media/sheep.a09e9eb8.png"
+  },
+  {
+    "revision": "b8b787a708660d63d8f87c9be7207de2",
+    "url": "/telefono-roto-frontend/static/media/sheep_draw.b8b787a7.png"
+  },
+  {
+    "revision": "d67c477229242637a068b6b74e54a7e2",
+    "url": "/telefono-roto-frontend/static/media/snail.d67c4772.png"
+  },
+  {
+    "revision": "cbe3eb42edbefe45968ca1fa5e2900c2",
+    "url": "/telefono-roto-frontend/static/media/snake.cbe3eb42.png"
+  },
+  {
+    "revision": "5e0fffe7a76e32eb05fef9f242a5580e",
+    "url": "/telefono-roto-frontend/static/media/spider.5e0fffe7.png"
+  },
+  {
+    "revision": "d33eb37908297b0e9d7a8f310f1b49c4",
+    "url": "/telefono-roto-frontend/static/media/whale.d33eb379.png"
+  }
+]);
